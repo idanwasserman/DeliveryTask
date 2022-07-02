083b0fe76dbc5547d243476083b3d20b8827798a
idanwasserman <idan.wasserman@s.afeka.ac.il>
2022-07-02 22:18:02 +0300

    Created basic spring web + mongodb project

---
+++ src/main/java/deliverytask/DeliveryTaskApplication.java
@@ -0,0 +1,13 @@
+package deliverytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/deliverytask/DeliveryTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package deliverytask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
